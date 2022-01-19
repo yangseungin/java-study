10a50473dba9861af4b840f24b76878d7ca17779
seungin yang <rhfpdk92@naver.com>
2022-01-20 01:04:14 +0900

    IT 서적 필터링

--- src/main/java/modern_java/behavior_parameterization/Book.java
+++ src/main/java/modern_java/behavior_parameterization/Book.java
@@ -1,13 +1,31 @@
 package modern_java.behavior_parameterization;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class Book {
     private String name;    //책이름
     private String author;  //저자
     private Integer year;   //출판년도
     private String field;   //분야
+
+    public Book(String name, String author, Integer year, String field) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.field = field;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getField() {
+        return field;
+    }
 }
--- src/main/java/modern_java/behavior_parameterization/Main.java
+++ src/main/java/modern_java/behavior_parameterization/Main.java
@@ -1,5 +1,6 @@
 package modern_java.behavior_parameterization;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -8,9 +9,21 @@ public class Main {
     public static void main(String[] args) {
         List<Book> books = asList(new Book("이펙티브 자바", "조슈아 블로크", 2018, "IT"),
                 new Book("모던 자바 인 액션", "라울-게이브리얼 우르마", 2019, "IT"),
-                new Book("이순신의 바다", "황현필", 2021, "역사"),
-                new Book("갈매기의 꿈", "리처드 바크", 2020, "소설")
+                new Book("이순신의 바다", "황현필", 2021, "History"),
+                new Book("갈매기의 꿈", "리처드 바크", 2020, "Fiction")
         );
+        List<Book> itBooks = filterITBooks(books);
+        
+    }
+
+    public static List<Book> filterITBooks(List<Book> inventory) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : inventory) {
+            if (book.getField().equals("IT")) {
+                result.add(book);
+            }
+        }
+        return result;
     }
 
 }
